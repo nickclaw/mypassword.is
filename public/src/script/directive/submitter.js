bc4647bf57444a296212c4671d2ccad75fd82e54
Nicholas Clawson <nickclaw@gmail.com>
2014-11-27 22:03:37 -0800

    Add submitter directive.

---
+++ public/src/script/directive/submitter.js
@@ -0,0 +1,34 @@
+angular.module('app').directive('submitter', [
+    '$http',
+    '$location',
+    function($http, $location) {
+
+        return {
+            restrict: 'E',
+            templateUrl: '/static/template/submit.html',
+            scope: {
+                redirect: '='
+            },
+            link: function($scope, elem, attr) {
+                $scope.entry = {
+                    reason: "",
+                    password: ""
+                };
+                $scope.saved = false;
+
+                $scope.save = function() {
+                    $http.post('/api/entry', $scope.entry)
+                        .success(function(data) {
+
+                            if ($scope.redirect) {
+                                $location.path('/entry/' + data._id);
+                            } else {
+                                $scope.entry = data;
+                                $scope.saved = true;
+                            }
+                        });
+                }
+            }
+        }
+    }
+])
