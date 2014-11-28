8c844552a25357ee4fa20e3e07d64edfa819e218
Nicholas Clawson <nickclaw@gmail.com>
2014-11-28 02:22:28 -0800

    Background-image styling

--- public/src/script/directive/entry.js
+++ public/src/script/directive/entry.js
@@ -6,6 +6,7 @@ angular.module('app').directive('entry', function() {
             entry: '='
         },
         link: function($scope, elem, attr) {
+            elem.attr('style', 'background-image: url(/static/image/' + Math.floor(Math.random() * 17 + 1) + '.jpg)');
             elem.addClass($scope.entry.view.type);
             elem.addClass($scope.entry.view.classes.join(' '));
             elem.attr('id', $scope.entry._id);
