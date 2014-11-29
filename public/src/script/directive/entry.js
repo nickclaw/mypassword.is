0defb1499179c6a4062c7d98913bced67efacfa5
Nicholas Clawson <nickclaw@gmail.com>
2014-11-28 16:32:30 -0800

    Partial background image working.

--- public/src/script/directive/entry.js
+++ public/src/script/directive/entry.js
@@ -6,7 +6,11 @@ angular.module('app').directive('entry', function() {
             entry: '='
         },
         link: function($scope, elem, attr) {
-            elem.attr('style', 'background-image: url(/static/image/' + Math.floor(Math.random() * 17 + 1) + '.jpg)');
+            if (!$scope.entry.view.background) {
+                elem.attr('style', 'background-image: url(/static/image/' + Math.floor(Math.random() * 17 + 1) + '.jpg)');
+            } else {
+                
+            }
             elem.addClass($scope.entry.view.type);
             elem.addClass($scope.entry.view.classes.join(' '));
             elem.attr('id', $scope.entry._id);
