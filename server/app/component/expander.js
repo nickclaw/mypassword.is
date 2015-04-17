8aebd39f01bb9b8f7b9f4473ca191b9187721e16
Nicholas Clawson <nickclaw@gmail.com>
2015-04-17 00:14:22 -0700

    Fix expander bugs

--- server/app/component/expander.js
+++ server/app/component/expander.js
@@ -3,19 +3,32 @@ import {Component} from 'react';
 
 export default class Expander extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            height: 0
+        };
+    }
+
     getDefaultProps() {
         return {
             expanded: true
         };
     }
 
+    componentWillReceiveProps(nextProps) {
+        let node = React.findDOMNode(this.refs.ruler);
+        this.setState({
+            height: node && nextProps.expanded ? node.clientHeight + 10 : 0
+        });
+    }
+
     render() {
-        let node = React.findDOMNode(this.refs.ruler),
-            className = "expander" + (this.props.expanded ? '' : ' expand-collapsed'),
-            style = { height: node ? node.clientHeight : 0 };
+        let className = "expander" + (this.props.expanded ? '' : ' expand-collapsed');
 
         return (
-            <div className={className} style={style}>
+            <div className={className} style={this.state}>
                 <div ref="ruler">
                     {this.props.children}
                 </div>
